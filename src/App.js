eb112271c7e737be1951798dea820cac752fba25
sasha-go <sasha.lfg@gmail.com>
2020-06-09 19:15:44 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main className='App'>
+      <h1>NetWorthy</h1>
+      <h2>Finally a simple app to track and grow your net worth.</h2>
+    </main>
+  );
+}
+
+export default App;
